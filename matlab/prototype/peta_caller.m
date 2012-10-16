68297c00c156aaf8b6fcf094a9f61b9a268e0a0a
Max Quinn <quinn.max@gmail.com>
2012-10-16 00:41:01 -0700

    peta_caller.m is now a little more portable, needing a little less modification when running on my mac vs a lab account...not really done right or anything though

--- matlab/prototype/peta_caller.m
+++ matlab/prototype/peta_caller.m
@@ -18,53 +18,45 @@ end
 
 function results = iterated_caller()
 
-    % add petacat prototype path
-        % addpath /Users/Max/Dropbox/Projects/Petacat/prototype
-        addpath /u/quinn/petacat-project/matlab/prototype
-    % add glimpse matlab wrapper path
-        % addpath /Users/Max/Dropbox/Projects/Petacat/petacat-project/matlab
-        addpath /u/quinn/petacat-project/matlab/
-    % add some tools (max's stuff)
-        addpath /u/quinn/petacat-project/matlab/prototype/tools
-    % add normalized cuts code
-        addpath('/u/quinn/petacat-project/matlab/normalized cuts code');
+    if strcmp(computer,'MACI64')
+        % add petacat prototype path
+        	addpath /Users/Max/Dropbox/Projects/Petacat/petacat-project/matlab/prototype
+        % add glimpse matlab wrapper path
+            addpath /Users/Max/Dropbox/Projects/Petacat/petacat-project/matlab
+        % image path
+            image_path = '/Users/Max/Dropbox/Projects/Petacat/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING';
+    else
+        % add petacat prototype path
+            addpath /u/quinn/petacat-project/matlab/prototype
+        % add glimpse matlab wrapper path
+            addpath /u/quinn/petacat-project/matlab/
+        % add some tools (max's stuff)
+            addpath /u/quinn/petacat-project/matlab/prototype/tools
+        % add normalized cuts code
+            addpath('/u/quinn/petacat-project/matlab/normalized cuts code');
+        % image path
+            image_path = '/u/quinn/mm-group/mm-group-images/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING';
+    end
     
-%     for n = 1:20
-%         fprintf('''/Users/Max/Documents/MATLAB/data/petacat_images/dog-walking-from-web/dog-walking-from-web%d.jpg''...\n',n);
-%     end
+    % get image names
+        return_directory = pwd;
+        cd(image_path);
+        images_fn = dir('*.jpg');
+        for fi = 1:length(images_fn)
+            fname_list{fi} = sprintf('%s/%s\n',image_path,images_fn(fi).name);
+        end
+        cd(return_directory);
+        clear return_directory;
 
-%     fname_list = {...
-%         '/Users/Max/Documents/MATLAB/data/petacat_images/dog-walking-from-web/dog-walking-from-web1.jpg'...
-%         '/Users/Max/Documents/MATLAB/data/petacat_images/dog-walking-from-web/dog-walking-from-web2.jpg'...
-%         '/Users/Max/Documents/MATLAB/data/petacat_images/dog-walking-from-web/dog-walking-from-web3.jpg'...
-%         '/Users/Max/Documents/MATLAB/data/petacat_images/dog-walking-from-web/dog-walking-from-web4.jpg'...
-%         '/Users/Max/Documents/MATLAB/data/petacat_images/dog-walking-from-web/dog-walking-from-web5.jpg'...
-%     };
-
-    fname_list = { ...
-        '/u/quinn/mm-group/mm-group-images/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING/dmh-dog-walking19.jpg' ...
-        '/u/quinn/mm-group/mm-group-images/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING/mdt-dog-walking12.jpg' ...
-        '/u/quinn/mm-group/mm-group-images/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING/mm-dog-walking10.jpg' ...
-        '/u/quinn/mm-group/mm-group-images/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING/mm-dog-walking20.jpg' ...
-        '/u/quinn/mm-group/mm-group-images/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING/mm-dog-walking30.jpg' ...
-        '/u/quinn/mm-group/mm-group-images/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING/mm-dog-walking40.jpg' ...
-        '/u/quinn/mm-group/mm-group-images/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING/mm-dog-walking50.jpg' ...
-        '/u/quinn/mm-group/mm-group-images/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING/mm-dog-walking60.jpg' ...
-        '/u/quinn/mm-group/mm-group-images/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING/mm-dog-walking75.jpg' ...
-        '/u/quinn/mm-group/mm-group-images/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING/mm-dog-walking90.jpg' ...
-        '/u/quinn/mm-group/mm-group-images/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING/mm-dog-walking107.jpg' ...
-        '/u/quinn/mm-group/mm-group-images/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING/mm-dog-walking109.jpg' ...
-        '/u/quinn/mm-group/mm-group-images/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING/mm-dog-walking192.jpg' ...
-        '/u/quinn/mm-group/mm-group-images/dog-walking-test-images-DO-NOT-USE-FOR-TRAINING/mm-dog-walking204.jpg' ...
-    };
+    
     
     method_options = {'random','salience','segmentation'};
     
     results = cell(3,1);
-    for fi = 1:length(fname_list)
-    for mi = 1:length(method_options)
-    % for fi = 1
-    % for mi = 2
+    % for fi = 1:length(fname_list)
+    % for mi = 1:length(method_options)
+    for fi = 1
+    for mi = 2
         
         tic;
         % if exist('pc','var'), clear pc; end
@@ -89,7 +81,7 @@ function results = iterated_caller()
             intersection_data{fi,mi} = pc.intersection;
             results{mi} = [results{mi}; pc.results];
             save(sprintf('pc_f_%d_m_%s',fi,method),'pc');
-            clear pc;
+            %clear pc;
 
             fprintf('%.3f, %s, %s\n',toc,method,fname);
         
